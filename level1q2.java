4b6b6136296de5c53b113f61145cf0a19489e8d2
davasaidhaniya <davasaidhaniya@gmail.com>
2026-04-09 19:58:22 +0530

    Level 1 q2

---
+++ level1q2.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class level1q2
+{
+    public static void main(String args[])
+    {
+        int phy=95,math=94,chem=96;
+        int avg=(95+94+96)/3;
+        System.out.println("Sam's average mark in PCM is "+avg);
+    }
+}
