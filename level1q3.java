7da876d70d58811c451413d65cb091357fa4b2db
davasaidhaniya <davasaidhaniya@gmail.com>
2026-04-09 20:00:31 +0530

    Level 1 q3

---
+++ level1q3.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class level1q3
+{
+    public static void main(String args[])
+    {
+        float km=10.8f,miles=0.0f;
+        miles=km/1.6f;
+        System.out.println("The distance "+km+" km in miles is "+miles+" miles");
+    }
+}
