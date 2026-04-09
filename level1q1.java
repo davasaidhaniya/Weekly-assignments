f5aa7616dbbf79ff18df10f159e594863bec998c
davasaidhaniya <davasaidhaniya@gmail.com>
2026-04-09 19:55:51 +0530

    Level 1 q1

---
+++ level1q1.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class level1q1
+{
+    public static void main(String args[])
+    {
+        int byear=2000,cyear=2024,age=0;
+        age=cyear-byear;
+        System.out.println("Harry's age in "+cyear+" is "+age);
+    }
+}
